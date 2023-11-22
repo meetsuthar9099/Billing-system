4680401162cc19cdef9fc2956f010667ea33b1bc
Meet Suthar <meet.suthar@codecrewinfotech.com>
2023-11-22 20:38:17 +0530

    First commit

---
+++ src/layouts/components/NavbarThemeSwitcher.vue
@@ -0,0 +1,16 @@
+<script setup>
+const themes = [
+  {
+    name: 'dark',
+    icon: 'bx-moon',
+  },
+  {
+    name: 'light',
+    icon: 'bx-sun',
+  },
+]
+</script>
+
+<template>
+  <ThemeSwitcher :themes="themes" />
+</template>
---
+++ src/pages/customer/customer-list.vue
@@ -0,0 +1,3 @@
+<template>
+    This is lising
+</template>
